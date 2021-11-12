62dcaa9f965afcd5bd6023bd568af046f5973fd3
Saheed Ibrahim <ibrahim.saheed2005@gmail.com>
2021-11-12 14:55:54 +0100

    add all six javascript concepts

---
+++ README.md
@@ -0,0 +1,14 @@
+# JAVA SCRIPT LEARNING WEBSITE
+ LANGUAGES USED
+
+* HTML
+* jQuery
+* java script
+* CSS
+* Bootstrap
+# Description
+#### This website is used to briefly explain some javascript related languages(html, css etc)
+#### as well as other javascript concepts.
+
+
+# Designed by Heritage and Ibrahim
---
+++ js/script.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+  $(".javascript").click(function() {
+    $(".show").toggle();
+    $(".hide").toggle();
+  });
+  $(".html").click(function(){
+    $(".release").toggle();
+    $(".cover").toggle();
+  });
+$(".bootstrap").click(function(){
+  $(".prove").toggle();
+    $(".conceal").toggle();
+});
+$(".jquery").click(function(){
+  $(".display").toggle();
+    $(".veil").toggle();
+});
+$(".css").click(function(){
+  $(".perform").toggle();
+    $(".shroud").toggle();
+});
+$(".var").click(function(){
+$(".visible").toggle();
+$(".mask").toggle();
+});
+});
